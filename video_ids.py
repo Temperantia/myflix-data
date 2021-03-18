74928785ff12408d7094c2b3929a84299a0bece5
Temperantia <adulorier@gmail.com>
2021-03-18 09:19:28 +0100

    fix

--- video_ids.py
+++ video_ids.py
@@ -64,7 +64,6 @@ def get_ids():
   for i in range(3040):  # 80_986_788 - 81 290 762 = 303,974
     index = 80_986_788 + i * 100
     args.append([index, 100, videos])
-  args = [args[0]]
   threads.threads(rangeCollect, args, 0.02, 'Scanning ids')
   print(error)
   print('Collected ' + str(len(videos)) + ' ids')
