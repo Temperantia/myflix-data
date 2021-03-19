e2e9fd356b68eb87f6de160eb94ecb45bf2c3ee5
Temperantia <adulorier@gmail.com>
2021-03-19 06:03:34 +0100

    fix

--- routine_stats.py
+++ routine_stats.py
@@ -116,8 +116,9 @@ def get_video_stats(videos: Dict[str, Any]):
   print('Upload')
   threads.threads(upload, [[video_id, video]
                            for video_id, video in videos.items()], 0, 'Uploading')
-  if 'categories' in video:
-    print(video['categories'])
+  for video_id, video in videos.items():
+    if 'categories' in video:
+      print(video['categories'])
   return
   print('Updating search tables')
   search = [{
